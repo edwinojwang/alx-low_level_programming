1c4b9df998e691580acaad71e35d37812c758531
edwinojwang <ojwangedwin98@gmail.com>
2022-12-11 01:46:56 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include <stdio.h> 
-/*
- * main - print a line of code using puts
- * Return: 0
+#include <stdio>
+/**
+ * main -Print a line of code using puts
+ *
+ * Return:0
 */
 int main(void)
 {
