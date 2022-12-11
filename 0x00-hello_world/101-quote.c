2c3a5b999a2e4ddfa89b10999c9cf16c25dfcf66
edwinojwang <ojwangedwin98@gmail.com>
2022-12-11 06:36:32 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
+ * main -Entry
  * Return:1
  */
-init main(void)
+int main(void)
 {
-	writer(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
