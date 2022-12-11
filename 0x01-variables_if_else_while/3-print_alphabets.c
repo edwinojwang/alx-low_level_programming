3222257e84f2500396f09a59a5137986ae749035
edwinojwang <ojwangedwin98@gmail.com>
2022-12-11 08:04:07 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha inlower and upper case
+ * Return:Always0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
