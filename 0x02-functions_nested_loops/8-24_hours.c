63d7280b2a6219c8803d8957302cef02f4127b7d
edwinojwang <ojwangedwin98@gmail.com>
2022-12-15 02:24:03 -0800

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
+ * jack_bauer - prints all the minutes of a day
  */
 void jack_bauer(void)
 {
@@ -11,11 +11,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((hour / 10) = '0');
-			_putchar((hour % 10) = '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((minute / 10) = '0');
-			_putchar((minute % 10) = '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
